da8b0de011fdb6c857beba84b46e7fd7f405275c
Glyson <83619472+Glysonn@users.noreply.github.com>
2023-05-02 23:48:00 -0300

    feat: Rotas GET da API
    Rota: /api/Link/GetAllLinks
    Descrição: Retorna todos os links cadastrado na base de dados.
    
    Rota: /api/Link/GetLinkByTitle
    Descrição: Retorna todos os links cujo o título contém o valor passado por parâmetro (string).
    
    Commits feito na branch backend:
    * Adicionando a propriedade _id do MongoDB
    
    A propriedade é readonly pois é somente para retornar o valor do _id que o mongo gera automaticamente (nao deve ser enviado)
    
    * Tratamento genérico para erros no servidor
    
    * feat: [GET] GetAllLinks endpoint
    
    Retorna todos os links cadastrado na base de dados
    
    * feat: [GET] GetLinkByTitle endpoint 
    
    Retorna todos os links cujo o título contém o valor enviado por parâmetro

--- backend/Controllers/LinkController.cs
+++ backend/Controllers/LinkController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using backend.Models;
 using backend.Context;
+using System.Net;
+using MongoDB.Bson;
+
 
 namespace backend.Controllers
 {
@@ -10,23 +13,64 @@ namespace backend.Controllers
     public class LinkController : ControllerBase
     {
         // aqui é a minha collection do banco de dados
-        IMongoCollection<Link> _links;
+        IMongoCollection<Link> _linksCollection;
+
 
         public LinkController(MongoConnection myMongoConnection, IConfiguration configuration)
         {
             var CollectionString = configuration["MongoCollections:BuscadorLinks:Link"];
 
             // recuperando a collection do banco e atribuíndo à variável _links (o tipo dela é IMongoCollection)
-            _links = myMongoConnection.context.GetCollection<Link>(CollectionString);
+            _linksCollection = myMongoConnection.context.GetCollection<Link>(CollectionString);
         }
 
-        // rota get somente para testar a API
-        [HttpGet]
-        public IActionResult rotaTeste()
+        // método para tratar os erros de forma genérica
+        private IActionResult HandleError(HttpStatusCode statusCode, string exceptionMessage)
         {
-            // somente para testar se a conexão está tudo funcionando 
-            var links = _links.AsQueryable().ToList();
-            return Ok(links);
+            return StatusCode((int)statusCode, new {
+                Error = Enum.GetName(typeof(HttpStatusCode), (int)statusCode),
+                StatusCode = statusCode,
+                Details = exceptionMessage });
+        }
+
+        // ENDPOINTS
+        [HttpGet]
+        [Route("GetAllLinks")]
+        public IActionResult GetAllLinks()
+        {
+            try
+            {
+                var links = _linksCollection.AsQueryable().ToList();
+                if (links.Count() == 0)
+                    return HandleError(HttpStatusCode.NotFound, "Oops! Ainda não há nenhum link cadastrado na nossa base de dados! :(");
+
+                return Ok(links);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetLinkByTitle/{title}")]
+        public IActionResult GetLinkByTitle(string title)
+        {
+            try
+            {
+                // RegEx para trazer todos que contém a string passada pelo parâmetro. O "i" serve para que seja independente de CaseSensitive
+                var filtro = Builders<Link>.Filter.Regex(lk => lk.Titulo, new BsonRegularExpression(title, "i"));
+                var linkBanco =_linksCollection.Find(filtro).ToList();
+                
+                if (linkBanco.Count() == 0 || linkBanco == null)
+                    return HandleError(HttpStatusCode.NotFound, $"Perdão, mas não achamos nenhuma página que contenha esse nome ({title})");
+
+                return Ok(linkBanco);
+            }
+            catch(Exception ex)
+            {
+                return HandleError(HttpStatusCode.InternalServerError, ex.Message);
+            }
         }
     }
 }
--- backend/Models/Link.cs
+++ backend/Models/Link.cs
@@ -1,9 +1,17 @@
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 namespace backend.Models
 {
     [BsonIgnoreExtraElements]
     public class Link
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] 
+        private string _id { get; set;}
+
+        // propriedade readonly do _id
+        public string Id { get {return _id;}}
+        
         [BsonElement("titulo")]
         public string Titulo { get; set; }
 
