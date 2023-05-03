b37d6de536cc5f38e9d5d6b39e62a3a7411ce9bf
Glysonn <kauaglyson@gmail.com>
2023-05-02 22:48:33 -0300

    Tratamento genérico para erros no servidor

--- backend/Controllers/LinkController.cs
+++ backend/Controllers/LinkController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using backend.Models;
 using backend.Context;
+using System.Net;
 
 namespace backend.Controllers
 {
@@ -19,6 +20,16 @@ namespace backend.Controllers
             // recuperando a collection do banco e atribuíndo à variável _links (o tipo dela é IMongoCollection)
             _links = myMongoConnection.context.GetCollection<Link>(CollectionString);
         }
+
+        // método para tratar os erros de forma genérica
+        public IActionResult HandleError(HttpStatusCode statusCode, string exceptionMessage)
+        {
+            return StatusCode((int)statusCode, new {
+                Error = Enum.GetName(typeof(HttpStatusCode), (int)statusCode),
+                StatusCode = statusCode,
+                Details = exceptionMessage });
+        }
+
 
         // rota get somente para testar a API
         [HttpGet]
